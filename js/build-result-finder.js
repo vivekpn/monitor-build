3cc950186aea56274cfe08934139405a0699a41f
unknown <vivekpn@users.noreply.github.com>
2015-07-10 04:11:37 +0530

    Exception handling was introduced.

--- js/build-result-finder.js
+++ js/build-result-finder.js
@@ -1,18 +1,22 @@
 
 var getStateOfBuild = function(buildURL){
+	var statusDivision=document.getElementById("status");
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", buildURL+"/lastCompletedBuild/api/json?pretty=true", true);
-	xhr.onreadystatechange = function() {
-	  if (xhr.readyState == 4) {
+	xhr.onerror= function(){
+		statusDivision.innerText = "Error occurred while fetching. Please try again";
+	}
+	xhr.onload = function(){
 		var lastCompletedBuild = JSON.parse(xhr.responseText);
-		document.getElementById("status").innerText = lastCompletedBuild.result;
-	  }
+		statusDivision.innerText = lastCompletedBuild.result;
+	}
+	xhr.onprogress = function(){
+		statusDivision.innerText = "Checking. Please wait...";		
 	}
 	xhr.send();
 }
 
 $( document ).ready(function() {
-    console.log( "ready!" );
 	var buildURL = "https://slabnode368.netact.nsn-rdnet.net/jenkins/job/ison_product_15.5_integration";
 	document.getElementById("buildURL").innerText = buildURL;
 	getStateOfBuild(buildURL);

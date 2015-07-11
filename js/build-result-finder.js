74c110c6036070728b039d2be2edad795798dc11
unknown <vivekpn@users.noreply.github.com>
2015-07-11 14:37:29 +0530

    Added a tabular display of build in the popup window.

--- js/build-result-finder.js
+++ js/build-result-finder.js
@@ -1,31 +1,41 @@
 
-var getStateOfBuild = function(buildURL){
-	var statusDivision=document.getElementById("status");
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", buildURL+"/lastCompletedBuild/api/json?pretty=true", true);
-	xhr.onerror= function(){
-		statusDivision.innerText = "Error occurred while fetching. Please try again";
-	}
-	xhr.onload = function(){
-		var lastCompletedBuild = JSON.parse(xhr.responseText);
-		statusDivision.innerText = lastCompletedBuild.result;
-	}
-	xhr.onprogress = function(){
-		statusDivision.innerText = "Checking. Please wait...";		
-	}
-	xhr.send();
-}
+var getStateOfBuild = function (buildURL) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", buildURL + "/lastCompletedBuild/api/json", true);
+    xhr.onerror = function () {
+        console.log("Error while fetching",buildURL);
+    };
+    xhr.onload = function () {
+        console.log("Loaded...",buildURL);
+        var response = JSON.parse(xhr.responseText);
+        processResponse(response);
+    };
+    xhr.onprogress = function () {
+        console.log("In Progress...",buildURL);
+    };
+    xhr.send();
+};
 
-$( document ).ready(function() {
-	var buildURL = null;
-	chrome.storage.sync.get("buildURL", function(items) {
-		if(items.buildURL){
-			buildURL = items.buildURL;
-			document.getElementById("buildURL").href = buildURL;
-			document.getElementById("buildURL").innerText = buildURL;		
-			getStateOfBuild(buildURL);
-		} else{
-			document.getElementById("information").innerText = "Please configure a right URL in the options.";
-		}
-	});
+var processResponse = function (response) {
+    var $row = $("#row_template").clone().removeClass('empty_row').removeAttr('id').html();
+    var buildDetails = {};
+    buildDetails.example_website=response.url;
+    buildDetails.example_build_name=response.fullDisplayName;
+    buildDetails.example_status=response.result;
+    buildDetails.example_time=response.id;
+    for (key in buildDetails) {
+        $row = $row.replace(key, buildDetails[key]);
+    }
+    $("#buld_information_table").append($row);
+};
+
+$(document).ready(function () {
+    chrome.storage.sync.get("buildURL", function (items) {
+        if (items.buildURL) {
+            var buildURL = items.buildURL;
+            getStateOfBuild(buildURL);
+        } else{
+            console.log("Build URL is not stored")
+        }
+    });
 });

c86ea2127d2932f83deda674749d2c482fe49654
unknown <vivekpn@users.noreply.github.com>
2015-07-12 00:35:12 +0530

    When wrong URL is entered an error message is shown.

--- js/options.js
+++ js/options.js
@@ -9,12 +9,16 @@ var save_options = function() {
         buildURLs: buildURLs
     }, function () {
         // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 1500);
+        changeSubmitStatus('Options saved.');
     });
+};
+
+var changeSubmitStatus = function(text){
+    var $status = $('#status');
+    $status.text(text);
+    setTimeout(function () {
+        $status.text('');
+    }, 1500);
 };
 
 // Restores select box and checkbox state using the preferences
@@ -32,7 +36,6 @@ var restore_options = function() {
 };
 
 $(document).ready(function () {
-    restore_options();
     $('#input_url_form').submit(function() {
         save_options();
         return false;
@@ -46,13 +49,21 @@ $(document).ready(function () {
     $(".remove").click(function() {
         $(this).parent().remove();
     });
+
+    restore_options();
+
 });
 //document.getElementById('save').addEventListener('click',
 //    save_options);
 
 var _createNewInputField = function(value){
     var $inputField = $('#input_template').clone(true).removeClass('template').removeAttr('id');
-    $inputField.find('input').val(value)
+    var $input = $inputField.find('input');
+    $input.val(value);
+    $input[0].oninvalid = function(){
+        changeSubmitStatus("Submit failed. Error in one or more fields.");
+    };
+//    $input[0].onchange=function(){try{setCustomValidity('')}catch(e){}};
     $inputField.insertBefore("#input_url_form > .add");
     return $inputField;
 };
